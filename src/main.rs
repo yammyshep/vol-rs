c26c8a7ff8581b425e2eda7123b239f618849b11
YammyShep <jdbuelow@gmail.com>
2022-03-09 23:32:23 -0600

    fixed infinite volume bug by limiting volume to 100%

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use hotkey;
 use pulsectl::controllers::SinkController;
 use pulsectl::controllers::AppControl;
 use x11_dl::keysym;
+use substring::Substring;
 
 // Customization options
 static DELTAVOL: f64 = 0.02;
@@ -40,18 +41,25 @@ pub fn register_hotkeys() {
     hotkeylistener.listen();
 }
 
-pub fn mut_app_vol(app_name: &str, vol: f64) {
+pub fn mut_app_vol(app_name: &str, mut vol: f64) {
     let mut audiohandler = SinkController::create().unwrap();
     let apps = audiohandler.list_applications()
     .expect("Failed to list applications");
 
     for app in apps.clone() {
         if app.name.as_ref().unwrap() == app_name {
+
+            // Clip max vol to 100%
+            let currentvol = app.volume.print().substring(3,6).replace(" ", "").parse::<i32>().unwrap();
+            if (currentvol + (vol*100.0) as i32 > 100) {
+                vol = (currentvol - 100) as f64 / 100.0;
+            }
+            
             if vol > 0.0 {
                 audiohandler.increase_app_volume_by_percent(app.index, vol);
             } else {
                 audiohandler.decrease_app_volume_by_percent(app.index, vol * -1.0);
-            } //TODO: fix infinite volume glitch real 2022 working 100% free virus free
+            }
         }
     }
 }
