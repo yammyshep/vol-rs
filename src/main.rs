d981f87ecde47b95cdcfbbe0d00314784e1f2c6c
YammyShep <jdbuelow@gmail.com>
2022-03-09 23:06:57 -0600

    added blank functions to allow ctrl+knob actions to differ

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,38 @@ use x11_dl::keysym;
 
 // Customization options
 static DELTAVOL: f64 = 0.02;
+static CKEY: u32 = keysym::XK_Pause;
+static CCKEY: u32 = hotkey::keys::INSERT;
 
 pub fn register_hotkeys() {
     let mut hotkeylistener: hotkey::Listener = hotkey::Listener::new();
 
     hotkeylistener.register_hotkey(
         0,
-        hotkey::keys::INSERT,
+        CCKEY,
         //hotkey::keys::ARROW_UP,
         || handle_vol_up()
-    )
-    .unwrap();
+    ).unwrap();
 
     hotkeylistener.register_hotkey(
         0,
-        keysym::XK_Pause,
+        CKEY,
         //hotkey::keys::ARROW_DOWN,
         || handle_vol_down()
-    )
-    .unwrap();
+    ).unwrap();
+
+    hotkeylistener.register_hotkey(
+        hotkey::modifiers::CONTROL,
+        CCKEY,
+        || handle_ctrl_vol_up()
+    ).unwrap();
+
+    hotkeylistener.register_hotkey(
+        hotkey::modifiers::CONTROL,
+        CKEY,
+        || handle_ctrl_vol_down()
+    ).unwrap();
+
     hotkeylistener.listen();
 }
 
@@ -38,7 +51,7 @@ pub fn mut_app_vol(app_name: &str, vol: f64) {
                 audiohandler.increase_app_volume_by_percent(app.index, vol);
             } else {
                 audiohandler.decrease_app_volume_by_percent(app.index, vol * -1.0);
-            }
+            } //TODO: fix infinite volume glitch real 2022 working 100% free virus free
         }
     }
 }
@@ -49,6 +62,14 @@ fn handle_vol_up() {
 
 fn handle_vol_down() {
     mut_app_vol("Spotify", DELTAVOL * -1.0);
+}
+
+fn handle_ctrl_vol_up() {
+
+}
+
+fn handle_ctrl_vol_down() {
+
 }
 
 fn main() {    
